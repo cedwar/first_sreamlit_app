abb7c3e780e9a0ff5b861f5d612bb76c874eb63b
cedwar <38218734+cedwar@users.noreply.github.com>
2022-04-14 16:49:12 +0100

    Update streamlit_app.py
    display fruits selected

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,14 @@ my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # put a pick list here 
-streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index),['Avocado', 'Stawberries'])
+fruits_selected = streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index))
+fruits_to_show = my_fruits_list.loc[fruits_selected]
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text(':blueberries: Omega 3 & Blueberry Oatmeal')
 
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
 
 
