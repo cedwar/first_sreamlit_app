65706a265afaaaddfd663a43729272fd7e6c8ce6
cedwar <38218734+cedwar@users.noreply.github.com>
2022-04-14 16:43:38 +0100

    Update streamlit_app.py
    add pick list - add missing comma

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 
 # put a pick list here 
-streamlit.multiselect('Pick some fruits:' list(my_fruit_list.index))
+streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index))
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
