a472a3203dd10edaab7f455fb6f8cf3541f40411
cedwar <38218734+cedwar@users.noreply.github.com>
2022-04-14 16:42:40 +0100

    Update streamlit_app.py
    add pick list

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit
 import pandas 
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 
+# put a pick list here 
+streamlit.multiselect('Pick some fruits:' list(my_fruit_list.index))
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
@@ -9,3 +11,5 @@ streamlit.header('Breakfast Menu')
 streamlit.text(':blueberries: Omega 3 & Blueberry Oatmeal')
 
 streamlit.dataframe(my_fruit_list)
+
+
