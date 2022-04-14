b0e33717eb970f721b7ac673a3925ccd0469b1c6
cedwar <38218734+cedwar@users.noreply.github.com>
2022-04-14 16:31:55 +0100

    Update streamlit_app.py
    add emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit
 streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text(':blueberries: Omega 3 & Blueberry Oatmeal')
